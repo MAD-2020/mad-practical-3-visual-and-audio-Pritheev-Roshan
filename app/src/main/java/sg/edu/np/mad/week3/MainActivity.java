a153f81ccb571ed401dc92c0126141133b3832e3
Pritheev-Roshan <s10193030@connect.np.edu.sg>
2020-05-09 03:15:08 -0700

    Week 3 submission

--- app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/week3/MainActivity.java
@@ -2,7 +2,11 @@ package sg.edu.np.mad.week3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView rabbidShow = findViewById(R.id.rabbidVideo);
+        rabbidShow.setVideoURI(Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.rabbid));
+        rabbidShow.start();
+
+        ImageView babyPicture = (ImageView) findViewById(R.id.babyImage);
+        babyPicture.setImageResource(R.drawable.cute_baby);
+
+        MediaPlayer babyLaugh = MediaPlayer.create(MainActivity.this,R.raw.baby_laughing);
+        babyLaugh.start();
     }
 }
